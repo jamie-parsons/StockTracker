ad63ce19fc247fa9a34938fb41f5466faed60172
jamie-parsons <jhelenparsons@gmail.com>
2021-04-20 20:00:17 -0600

    update models

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,10 @@ class Portfolio < ApplicationRecord
 
     belongs_to :user
 
+    #def self.check_db(ticker_symbol)
+    #    where(ticker: ticker_symbol).first
+    #end        TODO referenced to controller 
+    # may need to add new associations #271
+
+
 end
